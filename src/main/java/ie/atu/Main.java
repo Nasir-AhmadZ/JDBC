4b697229917ab55da6589e3bf154ab4969175c35
Nasir Ahmad <G00419630@atu.ie>
2024-03-14 12:33:28 +0000

    test connection

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -1,7 +1,24 @@
 package ie.atu;
-
+import java.sql.*;
 public class Main {
     public static void main(String[] args) {
-
+        Connection connection = null;
+        try {
+            //Load the driver class
+            Class.forName("com.mysql.cj.jbdc.Driver");
+            //Create a connection to the database, hardcoding values for now
+            connection = DriverManager.getConnection("jbdc:mysql://localhost3306/jdbc", "root", "password");
+            System.out.println("Connection made to connection pool");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        } finally {
+            //close the connection when finished
+            if (connection != null)
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+        }
     }
 }
